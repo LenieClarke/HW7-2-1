3e8b301ee6d6cd172d879b66ff42ede406d11ea7
Elena Kurbatova <lenie.clarke@mail.ru>
2020-08-31 04:38:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw7_2_1/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.hw7_2_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText addresExample;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addresExample = findViewById(R.id.addresExample);
+        Button searchBtn = findViewById(R.id.searchBtn);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                String string = addresExample.getText().toString();
+                char[] chars = string.toCharArray();
+                boolean addres = true;
+                for (char aChar : chars) {
+                    if (Character.isLetter(aChar)) {
+                        Uri uri = Uri.parse("geo:?q=" + string);
+                        intent.setData(uri);
+                        addres = false;
+                        break;
+                    }
+                }
+                if (addres) {
+                    Uri uri = Uri.parse("geo:" + string);
+                    intent.setData(uri);
+                }
+                startActivity(intent);
+            }
+        });
+    }
+}
